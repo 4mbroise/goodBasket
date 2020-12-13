6a8e1c442ff0685d0a153f2e01790a9501447eb7
CAI Yunfan <cai1015850388@gmail.com>
2020-12-13 19:51:33 +0100

    Interface Responsable s'applique nouveau bdd

--- IG/responsableIG/ResponsablePanel/producteursousig.h
+++ IG/responsableIG/ResponsablePanel/producteursousig.h
@@ -7,20 +7,45 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class ProducteurSousResponsable; }
 QT_END_NAMESPACE
 
+/**
+ * @brief InterfaceGraphique Producteur Sous Responsable
+ * @version 1
+ * @author CAI Yunfan
+ */
 class ProducteurSousResponsable : public QWidget
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Class Producteur sous Responsbale
+     * @param Qwidget widget de QT
+     */
     ProducteurSousResponsable(QWidget *parent = nullptr);
+
+    /**
+     * @brief Destructeur de la classe ProducteurSousResponsable
+     */
     ~ProducteurSousResponsable();
+
+    /**
+     * @brief Mise a jour le contenu dans interface Producteur
+     * @param QString contenu de String
+     * @param QString id de producteur
+     */
     void setData(const QString& a,const QString& id);
 
 
 private slots:
+    /**
+     * @brief button de confirmation
+     */
     void on_Confirmer_clicked();
 
 private:
+    /**
+     * @brief ui de interface Producteur
+     */
     Ui::ProducteurSousResponsable *ui;
 };
 #endif // WIDGET_H
